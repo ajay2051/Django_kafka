863fc3df910f7c44eaa4bf0b00ac2e6c56800971
Ajay Thakur <ajaythk.94@gmail.com>
2024-08-21 22:10:40 +0545

    model_created

---
+++ dj_kafka/models.py
@@ -0,0 +1,15 @@
+import uuid
+
+from django.db import models
+
+class FaceEmbed(models.Model):
+    id = models.AutoField(primary_key=True)
+    age = models.IntegerField()
+    emotion = models.CharField(max_length=50, null=True, blank=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'face_embed'
+        verbose_name = 'Face Embed'
+        ordering = ['-timestamp']
